3be86b49337b7d2f427fc905092f93124493df84
Gabriel Andrade <gabriel.andrade@outlook.com>
2022-08-20 16:23:13 -0300

    Setup TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          default: "#324b73 ",
+          hover: "#2d4468",
+          active: "#283c5c",
+        },
+        surface: "#121212",
+        background: "#000000",
+        white: "#ffffff",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      fontWeight: {
+        light: 300,
+        regular: 400,
+        bold: 700,
+      },
+      fontSize: {
+        title: "2rem",
+        subtitle: "1rem",
+        body: "1rem",
+      },
+    },
+  },
+  plugins: [],
+};
