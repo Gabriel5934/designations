1099501a7e89163d53c81c6ca3e72387fd9c1c8f
Gabriel Andrade <gabriel.andrade@outlook.com>
2022-10-13 15:10:53 -0300

    Translate designation title on upcoming

--- components/Upcoming.tsx
+++ components/Upcoming.tsx
@@ -3,7 +3,6 @@ import { useContext } from "react";
 import { v4 } from "uuid";
 
 import FirebaseContext from "../context/firebaseContext";
-import { Designation } from "../interfaces";
 import capitalizeFirstLetter from "../utils/capitalizeFirstLetter";
 
 interface UpcomingProps {
@@ -11,14 +10,14 @@ interface UpcomingProps {
 }
 
 export default function Upcoming({ personId }: UpcomingProps) {
-  const { designations, people } = useContext(FirebaseContext);
+  const { designations, people, titles } = useContext(FirebaseContext);
 
   const person = people[personId] ?? "";
 
   const formmatedDesignations = designations
     .map((designation) => ({
       person: designation.person,
-      title: capitalizeFirstLetter(designation.title),
+      title: capitalizeFirstLetter(titles[designation.title]),
       date: dayjs(designation.date.toDate()).format("DD/MM"),
     }))
     .filter((designation) => designation.person === personId);
